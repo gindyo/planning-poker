cbefe489ff14f06c3ccba0f0cf26ff3a7024e4a6
Dimitar Ginev <dimitar@dimitars-mbp.attlocal.net>
2019-03-13 00:27:30 -0500

    added some improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ng-learn';
+  gameName: string;
+  name: string;
+  game: Game;
+  game1Ref: AngularFirestoreDocument;
+  points: number;
+  role = "dev";
+  haveJoined: boolean;
+
+  constructor(private af: AngularFirestore) {
+    af.collection("/games")
+      .valueChanges()
+      .subscribe(v => {
+        this.game = new Game(v[0]);
+        this.haveJoined =
+          this.game.developers.some(d => this.name && d.name == this.name) ||
+          this.game.scrumMasters.some(sm => this.name && sm.name == this.name);
+        console.log(this);
+      });
+
+    this.game1Ref = this.af.collection("/games").doc("/game1");
+    this.game1Ref.get().subscribe(g => (this.game = new Game(g.data())));
+  }
+  flip() {
+    if (this.game.flipped) {
+      this.game.developers.forEach(d => (d.points = null));
+    }
+    this.game.flipped = !this.game.flipped;
+    this.game1Ref.set(this.game.toObj());
+  }
+  canJoin() {
+    return (
+      (this.role == "dev" &&
+        this.game.developers.every(d => d.name !== this.name)) ||
+      (this.role == "sm" &&
+        this.game.scrumMasters.every(sm => sm.name !== this.name))
+    );
+  }
+  join() {
+    if (this.canJoin()) {
+      if (this.role == "dev") {
+        this.game.developers.push({ name: this.name, points: 0 });
+      } else {
+        this.game.scrumMasters = this.game.scrumMasters || [];
+        this.game.scrumMasters.push({ name: this.name });
+      }
+      this.game1Ref.set(this.game.toObj());
+    } else {
+      this.haveJoined = true;
+    }
+  }
+  leave() {
+    this.game.developers = [];
+    this.game1Ref.set(this.game.toObj());
+  }
+  startNewGame() {
+    this.game = new Game(new Player(this.name));
+    this.game1Ref.set(this.game.toObj());
+  }
+  vote() {
+    this.game.developers.find(d => d.name === this.name).points = this.points;
+    this.game1Ref.set(this.game.toObj());
+  }
+}
+
+export class Game {
+  constructor(g) {
+    Object.assign(this, g);
+  }
+
+  flipped = false;
+  developers: Developer[] = [];
+  scrumMasters: Player[] = [];
+  toObj() {
+    return Object.assign({}, this);
+  }
+  setScrumMaster(scrumMaster: Player) {
+    this.scrumMasters.push(scrumMaster);
+  }
+}
+
+export class Player {
+  constructor(public name: string) {}
+}
+export class Developer extends Player {
+  points: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatListModule } from "@angular/material/list";
+const config = {
+  apiKey: "AIzaSyD1r9EGAT7n73Kf-qPXbIqFSZVWX9bqlMQ",
+  authDomain: "planning-pocker-6b6a6.firebaseapp.com",
+  databaseURL: "https://planning-pocker-6b6a6.firebaseio.com",
+  projectId: "planning-pocker-6b6a6",
+  storageBucket: "",
+  messagingSenderId: "1001464673332"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    MatListModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
